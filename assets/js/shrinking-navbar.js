9b87023c67c19a8eb805b618ee7659d0d9ff1e1b
Justin Gage <gagejustins@MacBook-Pro-5.local>
2018-06-02 19:33:34 -0400

    removed references to navbar logo

--- assets/js/shrinking-navbar.js
+++ assets/js/shrinking-navbar.js
@@ -2,10 +2,8 @@ $(document).ready(function() {
   $(window).on("scroll", function() {
     if ($(window).scrollTop() >= 130) {
       $(".navbar").addClass("compressed");
-      $("#logo-image").hide(300);
     } else {
       $(".navbar").removeClass("compressed");
-      $("#logo-image").show(300);
     }
   });
 });
